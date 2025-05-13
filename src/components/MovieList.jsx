ad2fa685725d429c87f7002376551a4dcfa61838
Михайло bc2024-6 <krasavaaabuster@gmail.com>
2025-05-13 17:36:32 +0300

    Виправлено передачу об'єкта movie у компонент MovieCard

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,15 +7,7 @@ function MovieList({ movies }) {
   return (
     <div className="movie-list">
       {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          title={movie.title}
-          description={movie.description}
-          genre={movie.genre}
-          poster={movie.poster}
-          date={movie.date}
-          time={movie.time}
-        />
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
   );
